7079de964e4a5d16786ace3e887e68bd97640f4c
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-07-09 19:54:52 +0400

    Initial Commit

---
+++ MoveSpritesAccelerometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoveSpritesAccelerometer
+//
+//  Created by Андрей Груненков on 09.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
